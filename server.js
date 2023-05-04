8753bdf70643b61b95cc01c7cbabf2688d76179c
fernando.dias <fernando.dias@iesde.com.br>
2023-05-04 10:04:27 -0300

    organizando a logica

--- server.js
+++ server.js
@@ -8,10 +8,47 @@ const io = new Server(httpServer)
 
 app.use(express.static('public'))
 
+let game = {}
 
 io.on("connection", (socket)=>{
-    console.log("test test"); 
-})
+
+    game[socket.id] = {
+        x: Math.floor(Math.random() * 10),
+        y: Math.floor(Math.random() * 10)
+    }
+    
+    socket.on('keyPress', (key)=>{
+        if(key=== "ArrowUp"){
+            if( game[socket.id].y - 1 >= 0 ){
+                game[socket.id].y --
+            }
+        }
+
+        if(key === "ArrowDown"){
+            if(game[socket.id].y + 1 <= 9 ){
+                game[socket.id].y ++
+            }
+        }
+
+        if(key == "ArrowRight"){
+            if(game[socket.id].x + 1 <= 9){
+                game[socket.id].x ++
+            }
+        }
+
+        if(key == "ArrowLeft"){
+            if(game[socket.id].x - 1 >= 0 ){
+                game[socket.id].x --
+            }
+        }
+        io.emit('game', game)
+    })
+
+    console.log(game);
+    io.emit('game', game)
+    
+}) 
+
 
 httpServer.listen(3000, ()=>{
     console.log("connection success")
